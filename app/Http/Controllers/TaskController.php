046c40a7ba9ec01c54a303df952a31f7aba6963e
Alex Zhdaniuk <webmaestro.alex@gmail.com>
2020-12-22 20:55:32 +0200

    mock task resource

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        return response()->json([
+            [
+                'title' => 'Run',
+                'completed' => 1,
+            ],
+            [
+                'title' => 'Walking',
+                'completed' => 0,
+            ],
+            [
+                'title' => 'Jump',
+                'completed' => 1,
+            ]
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return 'Task '.$id;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return 'Task '.$id . ' was deleted!';
+    }
+}
